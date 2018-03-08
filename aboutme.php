013706a0c57d006d89b1d590f0bde40f9a153c56
Nico Fricke <nico@nifni.net>
2018-03-08 10:06:32 +0100

    aboutme to config

--- aboutme.php
+++ aboutme.php
@@ -1,13 +1,11 @@
 <?php
 include('header.php');
+include('modules/config.php');
 ?>
 <div class="float" style="margin-bottom:3em">
 <p class="medium" style="margin-top:0.4em; margin-bottom:1em"><b>About Me</b></p>
 <div class="aboutme">
-
-Hello guys,
-my name is Nico Fricke and I am from Bremen/Germany.
-This node is hosted on a root-server by <a class="small" href="https://netcup.de">Netcup</a>. The node will be running 24/7 and (right now) I never intend to shut it down. If you have any questions regarding this node or anything else concerning Nano feel free to contact me at nano@nifni.net or on the Nano Discord at NiF#3422. If you wanna support me you can either donate to the address given at bottom or if you wanna get some webserver or a VPS from netcup you can contact me and I can give you referral keys which give you up to 30% discount and me 10% of everything you pay to netcup.
+<?php print($aboutMe); ?>
 </div>
 </div>
 
