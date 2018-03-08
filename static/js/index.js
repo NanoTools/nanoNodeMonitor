889ed5de8c35c96e61b1c7f20dfbf3330efb810f
Tobias Palmer <tobiaspalmer93@gmail.com>
2018-03-08 22:00:28 +0100

    only update if ajax is ready

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,27 @@
 var template;
 
+// get the template
 $.get('templates/index.hbs', function (data) {
     template=Handlebars.compile(data);
 
-    setInterval(updateStats, GLOBAL_REFRESH * 1000);
+    // and get the first data
     updateStats();
 }, 'html');
 
 function updateStats(){
+    // get the node information
     $.get('api.php')
     .done(function (apidata) {
+        // update the content
         $('#content').html(template(apidata));
     })
     .fail(function (apidata) {
+        // whoops! display the error
         $('#content').html(apidata.responseText);
         console.log('FAIL', apidata.responseText);
+    })
+    .always(function(){
+        // wait till the next update
+        setTimeout(updateStats, GLOBAL_REFRESH * 1000);
     });
 }
