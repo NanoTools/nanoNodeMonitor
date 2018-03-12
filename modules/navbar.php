721d2469c6de33cf4f5f47472d8c7a8501784df5
dbachm123 <dbachm123@gmail.com>
2018-03-12 17:56:04 +0000

    fixed main nano node monitor link

--- modules/navbar.php
+++ modules/navbar.php
@@ -1,6 +1,6 @@
 <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
   <div class="container">
-    <a href="../" class="navbar-brand">Nano Node Monitor</a>
+    <a href="index.php" class="navbar-brand">Nano Node Monitor</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
