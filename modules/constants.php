0013532038df8a22d0e75e193b3a9b8fac2d8950
dbachm123 <dbachm123@gmail.com>
2018-04-20 20:30:31 +0000

    added cache ttl as a separate parameter; extended some comments

--- modules/constants.php
+++ modules/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 // the project version
-define('PROJECT_VERSION', '1.4.0');
+define('PROJECT_VERSION', '1.4.1-dev');
 
 // project URL
 define('PROJECT_URL', 'https://github.com/NanoTools/nanoNodeMonitor');
