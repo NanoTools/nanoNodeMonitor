b64434b337a1a274aa32a1e6b129db36e36f66a7
iamreyne <me@alexreyne.com>
2018-04-28 00:15:07 -0400

    Added Dark Theme + Minor Bugfixes
     - Recreated dark theme to be compatible with theme system
     - Added dark theme logos
     - Made n-logo-theme around just text, not image
     - Added n-card-theme to cards, allowing greater customization for themes.
     - Removed old logos

--- modules/navbar.php
+++ modules/navbar.php
@@ -1,6 +1,6 @@
 <div class="navbar navbar-expand-lg n-navbar-theme fixed-top">
   <div class="container">
-    <a href="index.php" class="navbar-brand n-logo-theme"><img src="static/img/nano-mark-<?php echo $themeChoice; ?>.svg" alt="Nano Logo" style="height: 1em;"/> Nano Node Monitor</a>
+      <a href="index.php" class="navbar-brand"><img src="static/img/nano-mark-<?php echo $themeChoice; ?>.svg" alt="Nano Logo" style="height: 1em;"/><span class="n-logo-theme">Nano Node Monitor</span></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
