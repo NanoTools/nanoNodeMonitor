d62b3434c85c500f582c55322f638e300d1aa87c
Tobias Palmer <tobiaspalmer93@gmail.com>
2018-04-29 15:50:24 +0200

    merged #2

--- modules/header.php
+++ modules/header.php
@@ -8,20 +8,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="robots" content="noindex" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="static/css/bootstrap.min.css" media="screen">
         <link rel="stylesheet" href="static/css/fontawesome-all.min.css" media="screen">
-        <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,600,700,800,900" rel="stylesheet"> <!-- Nunito Font - Icarus -->
-        <link rel="stylesheet" href="static/css/<?php echo $themeChoice; ?>.css?v=<?php echo PROJECT_VERSION; ?>" media="screen">
-        <link rel="stylesheet" href="static/css/util.css">
-        <link rel="icon" href="static/img/logo-only-white.svg">
+        <link rel="stylesheet" href="static/css/main.css" media="screen">
+        <link rel="stylesheet" href="static/themes/<?php echo $themeChoice; ?>/css/theme.css?v=<?php echo PROJECT_VERSION; ?>" media="screen">
+        <link rel="icon" href="static/img/nano-mark-light.svg">
     </head>
     <body>
-        <script>
-        var init = [];
-        var GLOBAL_REFRESH = <?php echo $autoRefreshInSeconds; ?>;
-        var GLOBAL_DIGITS = <?php echo $nanoNumDecimalPlaces; ?>;
-        </script>
-
+    <script>
+    var init = [];
+    var GLOBAL_REFRESH = <?php echo $autoRefreshInSeconds; ?>;
+    var GLOBAL_DIGITS = <?php echo $nanoNumDecimalPlaces; ?>;
+    </script>
 
     <!--- add the navbar -->
     <?php include __DIR__ . '/navbar.php'; ?>
