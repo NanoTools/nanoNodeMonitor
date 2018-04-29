a3719018f272e68ac40adbb7f3eca5633655d640
dbachm123 <dbachm123@gmail.com>
2018-04-29 18:51:22 +0000

    minor adaptation of theme colors; clipboard icon colors; line spacing; font size

--- modules/footer.php
+++ modules/footer.php
@@ -2,9 +2,10 @@
   <p><?php echo getVersionInformation(); ?><br>
   GitHub: <a href="<?php echo PROJECT_URL; ?>" target="_blank">Source</a> | <a href="<?php echo PROJECT_URL . '/wiki'; ?>" target="_blank">Wiki</a> | <a href="<?php echo PROJECT_URL . '/wiki/API-Description'; ?>" target="_blank">API</a></p>
   <br>
-  <p class=small>Made by <a href="https://github.com/NanoTools" target="_blank">Nano Tools</a>.</p>
-  <p class=small>Donations to Nano Node Monitor: <a href="<?php echo getAccountUrl(NODEMON_DON_ACCOUNT, $blockExplorer); ?>" target="_blank"><?php echo NODEMON_DON_ACCOUNT; ?></a>
-  <button id="copyAccount" class="btn btn-sm btn-link btn-clipboard" data-clipboard-text="<?php echo NODEMON_DON_ACCOUNT; ?>" title="Copy"><i class="fas fa-clipboard fa-lg"></i></button>
+  <p class=small>Made by <a href="https://github.com/NanoTools" target="_blank">Nano Tools</a>.
+  <br>
+  Donations to Nano Node Monitor: <a href="<?php echo getAccountUrl(NODEMON_DON_ACCOUNT, $blockExplorer); ?>" target="_blank"><?php echo NODEMON_DON_ACCOUNT; ?></a>
+  <button id="copyAccount" class="btn btn-sm btn-link btn-clipboard-light" data-clipboard-text="<?php echo NODEMON_DON_ACCOUNT; ?>" title="Copy"><i class="fas fa-clipboard fa-lg"></i></button>
   </p>
 </footer>
 
