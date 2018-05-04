5c3117037d726f84b899e2a227780e2e615f6b44
dbachm123 <dbachm123@gmail.com>
2018-05-04 10:33:00 +0200

    version 1.4.5

--- modules/constants.php
+++ modules/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 // the project version
-define('PROJECT_VERSION', '1.4.5-dev');
+define('PROJECT_VERSION', '1.4.5');
 
 // project URL
 define('PROJECT_URL', 'https://github.com/NanoTools/nanoNodeMonitor');
