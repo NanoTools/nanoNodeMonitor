196f26e240ff3253dc1a3a64e38c5e3d9875415b
dbachm123 <dbachm123@gmail.com>
2018-05-10 20:35:45 +0000

    adapted address and explorer in footer for nano/banano

--- modules/footer.php
+++ modules/footer.php
@@ -14,22 +14,27 @@
     <br>   
 
 <?php
-  // switch Nano / Banano rep accounts
+  // switch Nano / Banano rep accounts & explorer
   $repAccount = NODEMON_REP_ACCOUNT;
+  $donAccount = NODEMON_DON_ACCOUNT;
+  $repExplorer = 'ninja';
+
   if ($themeChoice == "banano")
   {
     $repAccount = NODEMON_BAN_REP_ACCOUNT;
+    $donAccount = NODEMON_BAN_DON_ACCOUNT;
+    $repExplorer = 'banano';
   }
 ?>
 
-   <?php echo currencyNameFromTheme($themeChoice) ?> Representative: <a href="<?php echo getAccountUrl($repAccount, 'ninja'); ?>" target="_blank"><?php echo $repAccount; ?></a>
+   <?php echo currencyNameFromTheme($themeChoice) ?> Representative: <a href="<?php echo getAccountUrl($repAccount, $repExplorer); ?>" target="_blank"><?php echo $repAccount; ?></a>
    </small>
    <button id="copyAccount" class="btn btn-sm btn-link btn-clipboard-light" data-clipboard-text="<?php echo $repAccount; ?>" title="Copy"><i class="fas fa-clipboard fa-lg"></i></button>
    <br>
    <small>
-    Donations to <?php echo currencyNameFromTheme($themeChoice); ?> Node Monitor: <a href="<?php echo getAccountUrl(NODEMON_DON_ACCOUNT, $blockExplorer); ?>" target="_blank"><?php echo NODEMON_DON_ACCOUNT; ?></a>
+    Donations to <?php echo currencyNameFromTheme($themeChoice); ?> Node Monitor: <a href="<?php echo getAccountUrl($donAccount, $blockExplorer); ?>" target="_blank"><?php echo $donAccount; ?></a>
    </small>
-   <button id="copyAccount" class="btn btn-sm btn-link btn-clipboard-light" data-clipboard-text="<?php echo NODEMON_DON_ACCOUNT; ?>" title="Copy"><i class="fas fa-clipboard fa-lg"></i></button>
+   <button id="copyAccount" class="btn btn-sm btn-link btn-clipboard-light" data-clipboard-text="<?php echo $donAccount; ?>" title="Copy"><i class="fas fa-clipboard fa-lg"></i></button>
   </p>
 </footer>
 
