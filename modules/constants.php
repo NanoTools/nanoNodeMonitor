63a6e2dffbdb5739f56dec65338661ae2d63d920
dbachm123 <dbachm123@gmail.com>
2018-05-10 20:32:43 +0000

    added banano default accounts

--- modules/constants.php
+++ modules/constants.php
@@ -9,14 +9,17 @@ define('PROJECT_URL', 'https://github.com/NanoTools/nanoNodeMonitor');
 // URL to get version of latest release from github
 define('GITHUB_LATEST_API_URL', 'https://api.github.com/repos/NanoTools/nanoNodeMonitor/releases/latest');
 
-// rep account for Nano Node Monitor 
+// nano rep account for Nano Node Monitor 
 define ('NODEMON_REP_ACCOUNT', 'xrb_1f56swb9qtpy3yoxiscq9799nerek153w43yjc9atoaeg3e91cc9zfr89ehj');
 
-// rep account for Nano Node Monitor 
+// banano rep account for Nano Node Monitor 
 define ('NODEMON_BAN_REP_ACCOUNT', 'ban_1kxnxi5zurj6h7dfb87ik6hhu9yo63miyg6q1fjaxgnd1kknr5y5md4xwxoj');
 
-// donation account for Nano Node Monitor development
+// nano donation account for Nano Node Monitor development
 define ('NODEMON_DON_ACCOUNT', 'xrb_1nanomon9uycemhgonue4twmcqmsu7oxw43maro8amj751ozpus8r8gsic48');
+
+// baano donation account for Nano Node Monitor development
+define ('NODEMON_BAN_DON_ACCOUNT', 'ban_1kxnxi5zurj6h7dfb87ik6hhu9yo63miyg6q1fjaxgnd1kknr5y5md4xwxoj');
 
 // total number of characters for displaying Nano addresses including ellipsis
 define ('NANO_ADDR_NUM_CHAR', 17);
