52448121a66db577bf1c6f29edc6c49954b8dadb
Tobias Palmer <tobiaspalmer93@gmail.com>
2018-09-09 14:13:57 +0200

    ditch cmc widget for a qr code

--- modules/cmc_widget.php
+++ modules/cmc_widget.php
@@ -1,19 +1,12 @@
 <?php if ($currency == 'nano'): ?>
 
-<div class="coinmarketcap-currency-widget" 
-data-currencyid="1567" 
-data-base="<?php echo $cmcBaseCurrency; ?>" 
-data-secondary="<?php echo $cmcSecondaryCurrency; ?>" 
-data-ticker="<?php echo bool2string($cmcTicker); ?>" 
-data-rank="<?php echo bool2string($cmcRank); ?>" 
-data-marketcap="<?php echo bool2string($cmcMarketcap); ?>" 
-data-volume="<?php echo bool2string($cmcVolume); ?>" 
-data-stats="<?php echo $cmcBaseCurrency; ?>" 
-data-statsticker="<?php echo bool2string($cmcStatsticker); ?>"></div>
-<script src="https://files.coinmarketcap.com/static/widget/currency.js" async></script>
+<img id="monkey" src="https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=xrb:<?php echo $nanoNodeAccount; ?>&choe=UTF-8" 
+title="QR code for <?php echo $nanoNodeAccount; ?>"
+style="max-width:250px; display:block; margin: 0 0 0 auto;" />
+
 
 <?php elseif($currency == 'banano'): ?>
-<img id="monkey" src="http://www.monkeygen.com/image?address=<?php echo $nanoNodeAccount; ?>" 
+<img id="monkey" src="https://bananomonkeys.herokuapp.com/image?address=<?php echo $nanoNodeAccount; ?>" 
 title="monKey for <?php echo $nanoNodeAccount; ?>"
-style="max-width:250px; display:block; margin:0 auto;" />
+style="max-width:250px; display:block; margin: 0 0 0 auto;" />
 <?php endif; ?>
