ab222377e0a26da6ff10435f3fe90cd0fb69ac0d
dbachm123 <dbachm123@gmail.com>
2019-02-08 19:37:25 +0100

    version bump

--- modules/constants.php
+++ modules/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 // the project version
-define('PROJECT_VERSION', '1.4.14');
+define('PROJECT_VERSION', '1.5.0-dev');
 
 // project URL
 define('PROJECT_URL', 'https://github.com/NanoTools/nanoNodeMonitor');
