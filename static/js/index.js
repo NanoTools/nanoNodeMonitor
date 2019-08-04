f9a46e9d921dd5e205a787f28d7dfffe3bc742b6
Tobias Palmer <tobiaspalmer93@gmail.com>
2019-08-04 20:06:26 +0200

    show N/A delegators when not available

--- static/js/index.js
+++ static/js/index.js
@@ -35,6 +35,34 @@ init.push(function(){
     return hours+':'+minutes+':'+seconds;
   });
 
+  Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+
+    switch (operator) {
+        case '==':
+            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '!=':
+            return (v1 != v2) ? options.fn(this) : options.inverse(this);
+        case '!==':
+            return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+            return options.inverse(this);
+    }
+});
+
   axios.get('templates/index.hbs')
   .then(function (response) {
     template=Handlebars.compile(response.data);
