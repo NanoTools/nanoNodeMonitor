a35963fc203b3c279cf135deb64887c627488644
Tobias Palmer <tobiaspalmer93@gmail.com>
2019-08-26 08:53:57 +0200

    bump version

--- modules/constants.php
+++ modules/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 // the project version
-define('PROJECT_VERSION', '1.5.2');
+define('PROJECT_VERSION', '1.5.3');
 
 // project URL
 define('PROJECT_URL', 'https://github.com/NanoTools/nanoNodeMonitor');
