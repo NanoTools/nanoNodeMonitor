ff67594efb0ff20f949798922576ec86aed22dd9
Carlo Cervellin <32641633+crcarlo@users.noreply.github.com>
2021-06-03 20:50:26 +0200

    :recycle: Favicons  refactor (#147)
    * :recycle: Favicons  refactor
    
    * Fix wrong favicon path in browserconfig.xml
    
    * rename folder (lowercase)
    
    Co-authored-by: BitDesert <11021604+BitDesert@users.noreply.github.com>

--- modules/header.php
+++ modules/header.php
@@ -13,7 +13,26 @@
         <link rel="stylesheet" href="static/css/fontawesome-all.min.css?v=<?php echo PROJECT_VERSION; ?>" media="screen">
         <link rel="stylesheet" href="static/css/main.css?v=<?php echo PROJECT_VERSION; ?>" media="screen">
         <link rel="stylesheet" href="static/themes/<?php echo $themeChoice; ?>/css/theme.css?v=<?php echo PROJECT_VERSION; ?>" media="screen">
-        <link rel="icon" href="static/img/nano-mark-light.png">
+        <link rel="shortcut icon" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon.ico">
+        <link rel="icon" sizes="16x16 32x32 64x64" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon.ico">
+        <link rel="icon" type="image/png" sizes="196x196" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-192.png">
+        <link rel="icon" type="image/png" sizes="160x160" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-160.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-96.png">
+        <link rel="icon" type="image/png" sizes="64x64" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-64.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-16.png">
+        <link rel="apple-touch-icon" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-57.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-114.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-72.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-144.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-60.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-120.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-76.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-180.png">
+        <meta name="msapplication-TileColor" content="#FFFFFF">
+        <meta name="msapplication-TileImage" content="static/img/favicon/<?php echo currencyName($currency); ?>/favicon-144.png">
+        <meta name="msapplication-config" content="static/img/favicon/<?php echo currencyName($currency); ?>/browserconfig.xml">
         <meta property="og:title" content="<?php echo currencyName($currency); ?> Node Monitor - <?php echo $nanoNodeName; ?>">
         <meta property="og:description" content="Nano Node Monitor">
         <meta property="og:type" content="website">
